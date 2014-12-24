d30c4670e9290a3af73df210b13243a45c7f6136
vlaew <vlaew5@gmail.com>
2014-12-24 17:14:56 +0300

    testing

--- lib/generators/alchemy_tag_gallery_essence/views_generator.rb
+++ lib/generators/alchemy_tag_gallery_essence/views_generator.rb
@@ -1,11 +1,12 @@
 module AlchemyTagGalleryEssence
-  class ViewsGenerator < Rails::Generators::Base
+  class ViewsGenerator < Rails::Generators::NamedBase
 
     source_root File.expand_path('../../../app/views/alchemy/essences', __FILE__)
 
     desc ''
     def copy_views_files
       # create_file "config/initializers/initializer.rb", "# Add initialization content here"
+      puts 'test'
       filename_pattern = File.join self.class.source_root, '*.html.erb'
       Dir.glob(filename_pattern).map {|f| File.basename f}.each do |f|
         copy_file f, "app/views/alchemy/essences/#{f}"
